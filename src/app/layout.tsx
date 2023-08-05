4db8b0a0ab62b9802f2866e3a51844fb0151d881
youssef sk <alsarakibiy@gmail.com>
2023-08-05 13:24:15 +0300

    fixed route

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
-import "../styles/globals.css";
+import "./styles/globals.css";
 import type { Metadata } from "next";
-import Nav from "../components/Nav";
+import Nav from "./components/Nav";
 
 export const metadata: Metadata = {
   title: "Metaverse Modren",
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
-import About from "../sections/About";
-import Creater from "../sections/Creater";
-import Description from "../sections/Description";
-import Footer from "../sections/Footer";
-import Information from "../sections/Information";
-import Landing from "../sections/Landing";
-import Map from "../sections/Map";
-import World from "../sections/World";
+import About from "./sections/About";
+import Creater from "./sections/Creater";
+import Description from "./sections/Description";
+import Footer from "./sections/Footer";
+import Information from "./sections/Information";
+import Landing from "./sections/Landing";
+import Map from "./sections/Map";
+import World from "./sections/World";
 
 export default function Home() {
   return (
